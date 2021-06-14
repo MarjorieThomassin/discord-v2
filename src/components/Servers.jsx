02be2c3149926115f18a7b7d0b9f8f740f3f7717
MarjorieThomassin <79426116+MarjorieThomassin@users.noreply.github.com>
2021-06-14 16:29:20 +0200

    fix: grid problem and centering elements. Added media queries

--- src/components/Servers.jsx
+++ src/components/Servers.jsx
@@ -3,9 +3,9 @@ import '../CSS/Servers.css';
 
 function Servers() {
   return (
-    <>
+    <div className="serverContainer">
       <div className="TitleServer">
-        <h1>Your Severs</h1>
+        <h1>Your Servers</h1>
       </div>
       <div className="gridServer">
         <button className="serverButton" type="button">Unicorns</button>
@@ -14,8 +14,14 @@ function Servers() {
         <button className="serverButton" type="button">CrowCrew</button>
         <button className="serverButton" type="button">OtterGang</button>
         <button className="serverButton" type="button">FairyTale</button>
+        <button className="serverButton" type="button">FMAB</button>
+        <button className="serverButton" type="button">Death Note</button>
+        <button className="serverButton" type="button">One Piece</button>
+        <button className="serverButton" type="button">Bleach</button>
+        <button className="serverButton" type="button">Deadman Wonderland</button>
+        <button className="serverButton" type="button">Naruto</button>
       </div>
-    </>
+    </div>
   );
 }
 
